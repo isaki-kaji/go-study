9279f5f8d55d5816fbc3fae1842671cc5e8f2116
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-02-22 02:45:04 +0000

    mapのカンマokイディオム

--- src/learning-go/ch03/ex0316/main.go
+++ src/learning-go/ch03/ex0316/main.go
@@ -12,4 +12,18 @@ func main() {
 	fmt.Println(totalwins["ミュージシャンズ"]) // 1
 	totalwins["ナイツ"] = 3
 	fmt.Println(totalwins["ナイツ"])
+
+	m := map[string]int{
+		"hello": 5,
+		"world": 0,
+	}
+
+	v, ok := m["hello"]
+	fmt.Println(v, ok) //5 true
+
+	v, ok = m["world"]
+	fmt.Println(v, ok) // 0 true
+
+	v, ok = m["goodbye"]
+	fmt.Println(v, ok) // 0 true
 }
