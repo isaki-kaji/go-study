be9bbe884a2659b037aa48804ea63999681d9296
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-02-23 04:56:12 +0000

    switchでFizzBuzz

--- src/learning-go/ch04/ex0412/main.go
+++ src/learning-go/ch04/ex0412/main.go
@@ -18,4 +18,17 @@ func main() {
 		}
 		fmt.Println(i)
 	}
+
+	for i := 0; i < 100; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
